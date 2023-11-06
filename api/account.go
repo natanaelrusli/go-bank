47350124b7f75a2aa83193a38e8a671e3301f4d8
Nata Nael <natanael280198@gmail.com>
2023-11-06 18:16:27 +0700

    feat: (api): create account

---
+++ api/account.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/natanaelrusli/go-bank/db/sqlc"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
---
+++ api/server.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/natanaelrusli/go-bank/db/sqlc"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
