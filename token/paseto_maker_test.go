249855383bd26cdc3560690adff7600e7ed0508a
Nata Nael <natanael280198@gmail.com>
2023-11-10 11:02:14 +0700

    feat: add paseto token maker

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,50 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/natanaelrusli/go-bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+	require.NotZero(t, maker)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := time.Now().Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotZero(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, payload.Username, username)
+	require.WithinDuration(t, payload.ExpiredAt, expiredAt, time.Second)
+	require.WithinDuration(t, payload.IssuedAt, issuedAt, time.Second)
+}
+
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+// We don’t need the last test because the None algorithm just doesn’t exist in PASETO.
